0db9cb1595ad8b9ebe3e816ac221efdea016a34e
Bobby <bobby@bob.home>
2019-08-03 13:11:29 -0400

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,34 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        // The domain name of your Shopify shop. This is required.
+        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
+        // 'gatsby-source-shopify-test-shop.myshopify.com'.
+        shopName: "bobbyjenrowstore",
+  
+        // An API access token to your Shopify shop. This is required.
+        // You can generate an access token in the "Manage private apps" section
+        // of your shop's Apps settings. In the Storefront API section, be sure
+        // to select "Allow this app to access your storefront data using the
+        // Storefront API".
+        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
+        accessToken: "4779008798fc385821ee455623ca17ba",
+  
+        // Set verbose to true to display a verbose output on `npm run develop`
+        // or `npm run build`. This prints which nodes are being fetched and how
+        // much time was required to fetch and process the data.
+        // Defaults to true.
+        verbose: true,
+  
+        // Number of records to fetch on each request when building the cache
+        // at startup. If your application encounters timeout errors during
+        // startup, try decreasing this number.
+        paginationSize: 250,
+      },
+    },
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
